c108cb2461b9b97efc4fb00537b22ae22dbf0408
Aeris <135448749+Sernk@users.noreply.github.com>
2025-09-22 09:24:14 +0300

    Update IQuest.cs

--- Utilities/ModSupport/IQuest.cs
+++ Utilities/ModSupport/IQuest.cs
@@ -1,32 +1,56 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 
 namespace Bismuth.Utilities.ModSupport
 {
     public enum QuestPhase
     {
-        PreSkeletron, 
+        PreSkeletron,
         PostOriginalAlchemist,
         PostOriginalBabaYaga,
         PostOriginalDwarfBlacksmith
     }
+    public enum PostBossQuest
+    {
+        Null,               // Nothing
+        PostEoC,            // Eye of Cthulhu
+        PostBoss2,          // Brain of Cthulhu && Brain
+        PostSkeletron,      // Skeletron
+        PostQueenBee,       // Queen Bee
+        PostDeerclops,      // Deerclops
+        PostWoF,            // Wall of Flesh
+        PostQueenSlime,     // Queen Slime
+        PostMechBosses,     // All three mech bosses
+        PostTwins,          // The Twins
+        PostDestroyer,      // The Destroyer
+        PostSkeletronPrime, // Skeletron Prime
+        PostPlantera,       // Plantera
+        PostGolem,          // Golem
+        PostDukeFishron,    // Duke Fishron
+        PostEmpress,        // Empress of Light
+        PostCultist,        // Lunatic Cultist
+        PostMoonLord        // Moon Lord
+    }
     public interface IQuest
     {
         string UniqueKey { get; }
-        string NpcKey { get; } 
-        int Priority { get; }
+        string NpcKey { get; }
         string DisplayName { get; }
         string DisplayDescription { get; }
         string DisplayStage { get; }
-        bool ISManyEndings { get; }
+        int Progress { get; set; }
+        int Priority { get; }
         int CornerItem { get; }
+        bool ISManyEndings { get; }
         QuestPhase Phase { get; }
+        bool HasDefeated(PostBossQuest postBossQuest);
         string GetChat(NPC npc, Player player, int Itemcorneritem);
         string GetButtonText(Player player);
         void OnChatButtonClicked(Player player);
-        bool IsAvailable(Player player);
         void IsActiveQuestUIIcon(bool isAvailableQuest, bool isActiveQuest, SpriteBatch spriteBatch, NPC npc, Player player);
-        bool IsActive(Player player);
         void Notification(Player player, bool ISCompletedSuccessfully, bool ISQUESTACCEPTED);
+        bool IsAvailable(Player player);
+        bool IsActive(Player player);
+        bool IsCompleted(Player player) => false;
     }
 }
