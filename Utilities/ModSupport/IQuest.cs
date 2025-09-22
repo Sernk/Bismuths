1a721ec6a669b4598011a13883a21208a55ca8cc
Aeris <135448749+Sernk@users.noreply.github.com>
2025-09-23 02:06:38 +0300

    Update IQuest.cs

--- Utilities/ModSupport/IQuest.cs
+++ Utilities/ModSupport/IQuest.cs
@@ -1,5 +1,7 @@
+using Humanizer;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using static tModPorter.ProgressUpdate;
 
 namespace Bismuth.Utilities.ModSupport
 {
@@ -49,6 +51,8 @@ namespace Bismuth.Utilities.ModSupport
         void OnChatButtonClicked(Player player);
         void IsActiveQuestUIIcon(bool isAvailableQuest, bool isActiveQuest, SpriteBatch spriteBatch, NPC npc, Player player);
         void Notification(Player player, bool ISCompletedSuccessfully, bool ISQUESTACCEPTED);
+        int CompletedQuickSpawnItem(Player player, int IteID, int quantity = 1);
+        void CheckItem(Player player, int item_id, int need_an_item = 1, int how_many_items_to_spend = 1, string text = "", string textf = "",  int reward = 0, int stack = 0, bool IsNotification = true, bool IsQuestcompleted = true, int progres = 0);
         bool IsAvailable(Player player);
         bool IsActive(Player player);
         bool IsCompleted(Player player) => false;
